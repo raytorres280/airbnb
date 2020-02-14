4e6878d3715dd9e491eac713ef9c70041a0ff3ec
ray <raytorres280@gmail.com>
2020-02-14 00:19:32 -0500

    Initial Commit

---
+++ airbnb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  airbnb
+//
+//  Created by raymond torres on 2/14/20.
+//  Copyright © 2020 raymond-torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
